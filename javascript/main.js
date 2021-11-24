281faaf7bf67206556af277ffc140b9dc00df7b4
elan-sk <elan-sk@hotmail.com>
2021-11-23 21:59:52 -0500

    Cambio en main.sj

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 import {Usuario} from "./usuario.js";
 import {Examen} from "./examen.js";
 import {Pregunta} from "./pregunta.js";
-import * as controlador from "./controlador";
+
 
 
 const elan = new Usuario ("Elan Francisco"); // creación de un objeto de la clase usuario.
